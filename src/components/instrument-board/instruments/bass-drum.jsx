46bd408ee62c9c333c8922cecd771ff30a5712f6
Andreas <Hellberg.andreas@gmail.com>
2020-05-13 20:16:09 +0200

    slots
    slots for all instrumnets

--- src/components/instrument-board/instruments/bass-drum.jsx
+++ src/components/instrument-board/instruments/bass-drum.jsx
@@ -1,17 +1,37 @@
 import React from "react"
 import Slot from "../slots/slot"
 
-class BassDrum extends React.Component {
-  render() {
-    const slotNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    return (
-      <div className="slotRow">
-        {slotNumbers.map((number) => (
-          <Slot key={number} number={number} name="bd" />
-        ))}
-      </div>
-    )
-  }
+const BassDrum = () => {
+  const slotNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  const instruments = ["BD", "HH"]
+  return (
+    <div>
+      {instruments.map((instrument) => {
+        return (
+          <div className={"instrumentBoard__slotRow " + instrument}>
+            {slotNumbers.map((number) => (
+              <Slot key={number} number={number} name="bd" />
+            ))}
+          </div>
+        )
+      })}
+    </div>
+    // <div className="instrumentBoard__slotRowBD">
+    // </div>
+  )
 }
 
 export default BassDrum
+
+// class BassDrum extends React.Component {
+//   render() {
+//     const slotNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+//     return (
+//       <div className="slotRow">
+//         {slotNumbers.map((number) => (
+//           <Slot key={number} number={number} name="bd" />
+//         ))}
+//       </div>
+//     )
+//   }
+// }
---
+++ src/components/timekeeper/timeKeeper.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+class TimeKeeper extends React.Component {
+  render() {
+    return
+  }
+}
